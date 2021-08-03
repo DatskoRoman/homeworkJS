4a2652acc8e5a452407c6191da285707d75c2d15
DatskoRoman <pbk.datsko@gmail.com>
2021-08-03 17:33:22 +0300

    03.08 modified

--- mini project/JS/postInfo.js
+++ mini project/JS/postInfo.js
@@ -7,16 +7,22 @@ const url = new URL(location);
 const jsonUser = url.searchParams.get('post_info');
 const post = JSON.parse(jsonUser);
 const div_post = document.getElementById('post_info');
-div_post.innerText = jsonUser;
+
 
 
 fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
 .then(user => user.json())
 .then(users =>{
+    let h1_post = document.createElement('h1');
+    h1_post.innerText = `userId: ${post.userId} id:${post.id} title:${post.title}`;
+    div_post.appendChild(h1_post);
+    let h2_post = document.createElement('h2');
+    h2_post.innerText = `body: ${post.body}`;
+    div_post.appendChild(h2_post);
     let comment_block = document.getElementById('comments');
     for (let comment of users){
         let p_comment = document.createElement('p');
-        p_comment.innerText = comment.body;
+        p_comment.innerText = `Comment: ${comment.body}`;
         comment_block.appendChild(p_comment)
 
     }
--- mini project/JS/userInfo.js
+++ mini project/JS/userInfo.js
@@ -8,13 +8,29 @@ const url = new URL(location);
 const jsonUser = url.searchParams.get('user');
 const user = JSON.parse(jsonUser)
 const div_user = document.getElementById('info_user')
-let p_user = document.createElement('p');
-div_user.innerText = jsonUser;
+
+
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(responce => responce.json())
     .then(users => {
-        console.log(users);
+        let h1_user = document.createElement('h1');
+        h1_user.innerText = `${user.id} - ${user.name}- ${user.username}`;
+        div_user.appendChild(h1_user);
+        let p_user = document.createElement('p');
+        p_user.innerText = `email: ${user.email}`;
+        div_user.appendChild(p_user);
+        let p_user1 = document.createElement('p');
+        p_user1.innerText = `Address: street ${user.address.street}.  ${user.address.suite}, city ${user.address.city}, 
+        zipcode ${user.address.zipcode}, geo ${user.address.geo.lat} ${user.address.geo.lng}`;
+        div_user.appendChild(p_user1);
+        let p_user2 = document.createElement('p');
+        p_user2.innerText = `Phone: ${user.phone}. Website: ${user.website} `;
+        div_user.appendChild(p_user2);
+        let p_user3 = document.createElement('p');
+        p_user3.innerText = ` Company:${user.company.name}. CatchPhrase: ${user.company.catchPhrase}. Bs:${user.company.catchPhrase}. `;
+        div_user.appendChild(p_user3);
+
         let users_box = document.getElementsByClassName('post_user_block')[0];
         let main_div = document.createElement('div');
         main_div.classList.add('main_post_div')
